d954d53b5dca9b010afe818770f7359348cd9449
devcode25 <93724227+devcode25@users.noreply.github.com>
2023-05-03 11:50:49 -0300

    Add files via upload

---
+++ aula-array/README.md
@@ -0,0 +1,4 @@
+# Aula Array
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/terezadev0/pen/yLRPMxe](https://codepen.io/terezadev0/pen/yLRPMxe).
+
---
+++ aula-array/src/script.js
@@ -0,0 +1,30 @@
+// criando array
+const frutas = ["maçã", "banana"]
+                
+/*console.log(frutas.length)
+
+// Acessando itens
+
+frutas.forEach(function(item, indice, array){
+  console.log(item, indice, array)
+})*/
+
+//Acessar item
+
+/*const primeiro = frutas[0]
+
+const ultimo = frutas[frutas.length -1]
+
+console.log(ultimo)*/
+
+// Inserindo elementos
+
+const adicionar = frutas.push("laranja");
+
+frutas.splice(1, 0, "morango")
+
+
+
+console.log(frutas)
+
+
